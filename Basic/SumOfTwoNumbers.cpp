291a3675be9d693c9b63f497f87a6264da7d11cd
Tushar Gupta <tusharguptagps@gmail.com>
2024-11-11 21:19:12 +0530

    Create SumOfTwoNumbers.cpp

---
+++ Basic/SumOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter the value of A : ";
+    cin >> a;
+    cout << "Enter the value of B : ";
+    cin >> b;
+    
+    int sum = a+b;
+    cout << "Sum of A & B is : " << sum << endl;
+    
+    return 0;
+}
