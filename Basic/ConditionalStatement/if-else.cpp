d690b19743f7d5131aa1b95a8452ef94f136d9a1
Tushar Gupta <tusharguptagps@gmail.com>
2024-11-11 22:15:20 +0530

    Create if-else.cpp

---
+++ Basic/ConditionalStatement/if-else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+    cout << "Enter the value of n : ";
+    cin >> n;
+    
+    if(n>=0){
+        cout<< "n is Postiove"<< endl;
+    } else {
+        cout << "n is negative"<< endl;
+    }
+    
+    return 0;
+}
