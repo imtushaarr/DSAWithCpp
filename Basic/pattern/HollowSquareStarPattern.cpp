045281b8445541309c6358a8b9d2ac38bccc4089
Tushar Gupta <tusharguptagps@gmail.com>
2024-12-08 06:33:07 +0530

    Create HollowSquareStarPattern.cpp

---
+++ Basic/pattern/HollowSquareStarPattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row = 4;
+    int col = 4;
+    
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(i == 0 || i == row-1 || j == 0 || j == col-1){
+                cout<<"*";
+            } else{
+                cout << " ";
+            }
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
+
+// output
+// ****
+// *  *
+// *  *
+// ****
